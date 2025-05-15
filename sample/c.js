0fb8872b7c110730b3393959cd85f5b21478a709
BogaRasagna <159411875+BogaRasagna@users.noreply.github.com>
2025-05-15 14:12:52 +0530

    Add files via upload

---
+++ sample/c.js
@@ -0,0 +1,9 @@
+// This JavaScript code is not used in the current example,
+// as the button's alert is directly in the HTML.
+// But it shows how you would potentially handle events in JS.
+//
+// function handleClick() {
+//   alert("Button Clicked!");
+// }
+//
+// document.querySelector("button").addEventListener("click", handleClick);
